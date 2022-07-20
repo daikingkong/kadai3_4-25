a5f3d14409d8b6afb2230b85a387ded4ce0eb790
EC2 Default User <daikingkong>
2022-07-20 04:35:22 +0000

    [Add] 4-25 投稿済みのbookの編集画面と機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:index, :show, :edit, :create, :destroy]
-  resources :users, only: [:index, :show, :edit, :create]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:create, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
