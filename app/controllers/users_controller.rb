bdbd6582b55851fe9039cab08560d5869ee1ffba
EC2 Default User <daikingkong>
2022-07-20 06:24:49 +0000

    [Add] 4-25 ユーザー情報の編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @book = book.new(book_params)
+    @book = book.new(user_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
@@ -19,11 +19,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
 
-  def book_params
-    params.require(:book).permit(:title, :body)
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
